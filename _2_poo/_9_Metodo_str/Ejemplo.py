22dcb466ae1363b76443510762b18753369b27ea
“Ricardo” <richardflglz12@icloud.com>
2025-04-30 17:57:40 -0600

    Si

---
+++ _2_poo/_9_Metodo_str/Ejemplo.py
@@ -0,0 +1,24 @@
+class DatosPersonales:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre= nombre
+        self.apellido=apellido
+        self.edad=edad
+
+    def getNombre(self):
+        return self.nombre
+
+    def getApellido(self):
+        return self. apellido
+
+    def getEdad(self):
+        return self.edad
+
+    def corto(self):
+        return self.nombre+" "+self.apellido+" "+self.edad+" años ";
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido + " " + self.edad + " años ";
+
+if __name__ == '__main__':
+    datos=DatosPersonales("Hector", "Flores", "32")
+    print(datos)
